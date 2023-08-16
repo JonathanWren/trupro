20caa5e6ac6184ac3386a017062001ded89de120
Caleb-Agnes <Caleb.John.Agnes@gmail.com>
2023-08-16 14:20:55 +0100

    Dependencies and role location update

--- main/rolelocation.js
+++ main/rolelocation.js
@@ -6,6 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 import styles from '../component.style.js';
 import { useDispatch } from 'react-redux';
 import { updateNextLocation } from '../redux/profileSlice.js';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const RoleLocation = () => {
 
@@ -13,24 +14,40 @@ const RoleLocation = () => {
     const dispatch = useDispatch();
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Location</Text>
-            <Text style={styles.text}>Trupro is initially launching for users in London, UK. Further locations will be added soon.</Text>
-            <Text
-                    style={styles.input}
-                >London, UK
-            </Text>
+        <View style={importedstyles.container}>
+            <Text style={importedstyles.heading}>Location</Text>
+            
+            <GooglePlacesInput/>
             <TouchableOpacity
-                style={styles.button}
+                style={importedstyles.button}
                 onPress={() => {
-                    dispatch(updateNextLocation({location: "London, UK"}));
+                    dispatch(updateNextLocation({location: }));
                     nav.navigate("Next Move");
                 }}
             >
-                <Text style={styles.buttonText}>Save</Text>
+                <Text style={importedstyles.buttonText}>Save</Text>
             </TouchableOpacity>
         </View>
     );
 }
+
+const GooglePlacesInput = () => {
+    return (
+      <View width={'100%'} height={'90%'}>
+      <Text style={importedstyles.text}>Select your location</Text>
+      <GooglePlacesAutocomplete
+        placeholder='Search'
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+        }}
+        query={{
+          key: 'AIzaSyAzvm4cAz7g2U-NnaGqU3S5Vkqv0oinOhQ',
+          language: 'en',
+        }}
+      />
+      </View>
+    );
+  };
 
 export default RoleLocation;
