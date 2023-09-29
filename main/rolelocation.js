2f2daab7cd6b5397be5633475077f45447c28805
Jonathan Wren <jonathan.wren@higherme.app>
2023-09-29 17:46:46 +0100

    Tidyup to code

--- main/rolelocation.js
+++ main/rolelocation.js
@@ -1,13 +1,12 @@
-//Screen allowing user to select their role. Currently only support product management roles.
+//Screen allowing user to select their location.
 
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from '../component.style.js';
 import { useDispatch } from 'react-redux';
 import { updateNextLocation } from '../redux/profileSlice.js';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { useState } from 'react';
 import secretconfig from '../main/secretconfig.js';
 import * as Location from 'expo-location';
 
@@ -16,58 +15,51 @@ const RoleLocation = () => {
     const nav = useNavigation();
     const dispatch = useDispatch();
   
-    const location = null;
-    const [errorMsg, setErrorMsg] = useState(null);
-    let text = '';
-  
-    if (errorMsg) {
-      text = errorMsg;}
-
-      //this error message will not be displayed to the user currently
-  
     return (
       <View style={styles.container}>
         <Text style={styles.heading}>Location</Text>
-  
         <View width={'100%'} height={'90%'}>
           <GooglePlacesAutocomplete
-          placeholder='Enter Location'
+            placeholder='Enter Location'
             fetchDetails={true}
-            onPress={(data, details = null) => {
-              if (data.description === 'Current Location') {
-                //fetching location
-                (async () => {
-                  text = 'Loading...';
-                  let { status } = await Location.requestForegroundPermissionsAsync();
-                  if (status !== 'granted') {
-                    setErrorMsg('Permission to access location was denied');
-                    return;
-                  }
-            
-                  let location = await Location.getCurrentPositionAsync({});
-                    dispatch(updateNextLocation({
-                        locationName: 'Current Location',
-                        locationLat: location.coords.latitude,
-                        locationLng: location.coords.longitude,
-                    }))
-                })();}              
-              else {
-                //set the location of item
-                dispatch(updateNextLocation({
-                    locationName: data.description,
-                    locationLat: details.geometry.location.lat,
-                    locationLng: details.geometry.location.lng,
-                }))
+            onPress={ (data, details = null) => {
+                if (data.description === 'Current Location') {
+                  //fetching location
+                  (async () => {
+                      const { status } = await Location.requestForegroundPermissionsAsync();
+                      if (status !== 'granted') {
+                        alert('Permission to access location was denied');
+                        return;
+                      }
+                
+                      const location = await Location.getCurrentPositionAsync({});
+                      dispatch(
+                        updateNextLocation({
+                          locationName: 'Current Location',
+                          locationLat: location.coords.latitude,
+                          locationLng: location.coords.longitude,
+                        })
+                      )
+                    }
+                  )();
+                }              
+                else {
+                  //set the location of item
+                  dispatch(
+                    updateNextLocation({
+                      locationName: data.description,
+                      locationLat: details.geometry.location.lat,
+                      locationLng: details.geometry.location.lng,
+                    })
+                  )
+                }
+                nav.navigate("Next Move");
               }
-              nav.navigate("Next Move");
-            }}
+            }
             query={{
               key: secretconfig.GooglePlacesKey,
               language: 'en',
               types: 'geocode',
-            }}
-            textInputProps={{
-              defaultvalue: location,
             }}
             predefinedPlaces={[{description: 'Current Location', geometry: {location: {lat: 0, lng: 0}}}]}
           />
